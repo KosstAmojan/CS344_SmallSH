55a4e31ed39de8d346e1902114aa95eea93475c6
Matthew Norwood <mnorwood8066@gmail.com>
2022-02-04 19:15:30 -0500

    Parsed user input into struct for command, arguments, input file, output file, and background processing.

--- main.h
+++ main.h
@@ -23,6 +23,14 @@
 
 // Struct definitions here //
 
+struct command_data {
+    char *command;
+    char *argumentList;
+    char *input_file;
+    char *output_file;
+    int run_in_background;
+};
+
 ssize_t pidDigitCount(pid_t process_id){
     ssize_t pidDigitCnt = 0;
 
@@ -51,10 +59,10 @@ char* pidConvert(pid_t process_id){
 
 }
 
-void processUserInput(const char *inputString, size_t bytes_read){
+int processUserInput(const char *inputString, size_t bytes_read){
 
     if (strncmp("exit", inputString, bytes_read) == 0) {
-        // run the exit command.
+        return 0;
     }
 
 
@@ -88,6 +96,103 @@ void pidExpand(char* inputString, size_t pidDigitCnt, const char *pidString){
 
 }
 
+void parseCommand(char *usr_input, struct command_data *command_data){
+
+    char *saveptr;
+    char *usr_input_cpy = calloc(strlen(usr_input) + 1,sizeof(char));
+    size_t run_token_len = 0;
+    int str_trail = 1;
+
+    strcpy(usr_input_cpy, usr_input);
+
+    char *token = strtok_r(usr_input_cpy, " \0", &saveptr);
+    command_data->command = calloc(strlen(token) + 1, sizeof(char));
+    strcpy(command_data->command, token);
+    run_token_len += strlen(token) + 1;
+
+    while (usr_input[run_token_len ] != '\0' && (void *)token != NULL) {
+        token = strtok_r(NULL, "<>&\0", &saveptr);
+
+        // input file
+        if (usr_input[run_token_len] == '<' && usr_input[run_token_len + 1] == ' ') {
+
+            // make the trailing space(s) null terminator(s) (if there are a trailing space(s)).
+            while (token[strlen(token) - str_trail] == ' '){
+                token[strlen(token) - 1] = '\0';
+                str_trail++;
+                run_token_len++;
+            }
+            str_trail = 1;
+
+            // advance the pointer past the first space
+            while (token[0] == ' ') {
+                token++;
+                run_token_len++;
+            }
+
+            command_data->input_file = calloc(strlen(token) + 1, sizeof(char));
+            strcpy(command_data->input_file, token);
+            run_token_len++;
+        }
+
+        // output file
+        else if (usr_input[run_token_len] == '>' && usr_input[run_token_len + 1] == ' ') {
+
+            // make the trailing space(s) null terminator(s) (if there are a trailing space(s)).
+            while (token[strlen(token) - str_trail] == ' '){
+                token[strlen(token) - 1] = '\0';
+                str_trail++;
+                run_token_len++;
+            }
+            str_trail = 1;
+
+            // advance the pointer past the first space
+            while (token[0] == ' ') {
+                token++;
+                run_token_len++;
+            }
+
+            command_data->output_file = calloc(strlen(token) + 1, sizeof(char));
+            strcpy(command_data->output_file, token);
+            run_token_len += 1;
+        }
+
+        // background process
+        else if (usr_input[run_token_len ] == '&' && (usr_input[run_token_len + 1] == ' ' || usr_input[run_token_len + 1]== '\0')) {
+
+            command_data->run_in_background = 1;
+            run_token_len++;
+        }
+
+        // command arguments
+        else if (token != NULL && ((usr_input[run_token_len] != '<' && usr_input[run_token_len + 1] != ' ') ||
+                                    (usr_input[run_token_len] != '>' && usr_input[run_token_len + 1] != ' ') ||
+                                    usr_input[run_token_len ] == '&' && usr_input[run_token_len + 1] != ' ') ){
+
+            // make the trailing space(s) null terminator(s) (if there are a trailing space(s)).
+            while (token[strlen(token) - str_trail] == ' '){
+                token[strlen(token) - 1] = '\0';
+                str_trail++;
+                run_token_len++;
+            }
+            str_trail = 1;
+
+            // advance the pointer past the first space
+            while (token[0] == ' ') {
+                token++;
+                run_token_len++;
+            }
+
+            command_data->argumentList = calloc(strlen(token) + 1, sizeof(char));
+            strcpy(command_data->argumentList, token);
+            run_token_len++;
+        }
+        if (token != NULL) {
+            run_token_len += strlen(token);
+        }
+    }
+}
+
 void shellUserInput(void) {
     // This function, called by main.c, handles basic user input/option selection. It then calls the appropriate
     // helper functions as necessary to complete file selection and processing for the user.
@@ -96,8 +201,9 @@ void shellUserInput(void) {
 
         char **usr_input = malloc(sizeof(char *) * MAX_LINE);
         char *usr_input_str = malloc(sizeof(char) * MAX_LINE);
+        struct command_data *curr_command = malloc(sizeof(struct command_data));
         char *pidString = NULL;
-        int n = 0, i = 0;
+        int n = 0, i = 0, status = 0;
         size_t max_line1 = MAX_LINE, *max_line = &max_line1, bytes_read = 0, alloc_mem = sizeof(char) * MAX_LINE,
                             pidDigitCnt = 0;
         pid_t shell_process_id = getpid();
@@ -149,19 +255,33 @@ void shellUserInput(void) {
            }
            n++;
        }
+       // parse input into command struct.
+       parseCommand(usr_input_str, curr_command);
 
-        processUserInput(usr_input_str, bytes_read);
-        printf("The string the user entered was: '%s' and it was %d bytes long.\n", usr_input_str, (int)bytes_read);
+       printf("command: %s\narguments: %s\ninput: %s\noutput: %s\nbackground %d\n",
+              curr_command->command, curr_command->argumentList, curr_command->input_file, curr_command->output_file,
+              curr_command->run_in_background);
 
-        i++;
-        if (i == 10) {
+       status = processUserInput(usr_input_str, bytes_read);
+       printf("The string the user entered was: '%s' and it was %d bytes long.\n", usr_input_str, (int)bytes_read);
 
-            free(pidString);
-            free(*usr_input);
-            free(usr_input_str);
-            free(max_line);
-            break;
-        }
+       if (status == 0){
+           return;
+       }
+
+       i++;
+       if (i == 10) {
+           free(curr_command->command);
+           free(curr_command->argumentList);
+           free(curr_command->input_file);
+           free(curr_command->output_file);
+           free(curr_command);
+           free(pidString);
+           free(*usr_input);
+           free(usr_input_str);
+           free(max_line);
+           break;
+       }
 
     }
 }
