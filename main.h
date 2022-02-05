00ff586cacecc5066a9500814c23a2876e7471c4
KosstAmojan <norwooma@oregonstate.edu>
2022-02-05 02:31:29 -0500

    Finished cd, exit. Added signal handler for SIGTERM for main process. Still need to do status, exec()/fork()/waitpid(), input/output redirection, foreground/background execution, and SIGINT/SIGSTP.

--- main.h
+++ main.h
@@ -17,9 +17,11 @@
 #include <sys/fcntl.h>
 #include <time.h>
 #include <unistd.h>
+#include <signal.h>
 
 // Macro definitions here //
 #define MAX_LINE 2048
+#define MAX_PATH 4096
 
 // Struct definitions here //
 
@@ -59,13 +61,63 @@ char* pidConvert(pid_t process_id){
 
 }
 
-int processUserInput(const char *inputString, size_t bytes_read){
+int changeDirectory(struct command_data *curr_command) {
 
-    if (strncmp("exit", inputString, bytes_read) == 0) {
-        return 0;
+    char pwd[MAX_PATH] = {};
+    char cwd[MAX_PATH] = {};
+    char exp_tilde[MAX_PATH] = {};
+
+    printf("%s\n",getcwd(pwd,MAX_PATH));
+
+
+    if (curr_command->argumentList == NULL) {
+        // change working directory to directory stored in Home variable.
+        if (getenv("HOME") != NULL) {
+            if (chdir(getenv("HOME")) != 0){
+                perror("Error changing wd to HOME.");
+            };
+        }
     }
 
+    else if (curr_command->argumentList[0] == '~') {
+        if (getenv("HOME") != NULL) {
+            strcat(exp_tilde, getenv("HOME"));
+            strcat(exp_tilde, curr_command->argumentList+1);
+            if (chdir(exp_tilde) != 0){
+                perror("Invalid path.");
+            };
+        }
+    }
 
+    else if (chdir(curr_command->argumentList) != 0) {
+        perror("Invalid path.");
+        return -1;
+    }
+    printf("%s\n",getcwd(cwd,MAX_PATH));
+    return 0;
+
+}
+
+void killAll(void){
+    // 0 will kill all the processes in the parent's process group. SIGTERM allows the processes to perform clean up.
+    kill(0,SIGTERM);
+
+}
+int processUserInput(struct command_data *curr_command){
+
+    int ret_status = 0;
+
+    if (strcmp(curr_command->command,"exit") == 0){
+        killAll();
+        return 1;
+    }
+
+    else if (strcmp(curr_command->command,"cd") == 0){
+        if (changeDirectory(curr_command) == 0) {
+            return ret_status;
+        }
+        else return -1;
+    }
 }
 
 void pidExpand(char* inputString, size_t pidDigitCnt, const char *pidString){
@@ -98,10 +150,13 @@ void pidExpand(char* inputString, size_t pidDigitCnt, const char *pidString){
 
 void parseCommand(char *usr_input, struct command_data *command_data){
 
+
+
     char *saveptr;
     char *usr_input_cpy = calloc(strlen(usr_input) + 1,sizeof(char));
     size_t run_token_len = 0;
     int str_trail = 1;
+
 
     strcpy(usr_input_cpy, usr_input);
 
@@ -196,12 +251,28 @@ void parseCommand(char *usr_input, struct command_data *command_data){
 void shellUserInput(void) {
     // This function, called by main.c, handles basic user input/option selection. It then calls the appropriate
     // helper functions as necessary to complete file selection and processing for the user.
+    // Initialize SIGINT_action struct to be empty
+
+    // Borrowing signal struct code from lecture materials.
+    struct sigaction ignore_action = {0};
+
+    // The ignore_action struct as SIG_IGN as its signal handler
+    ignore_action.sa_handler = SIG_IGN;
+
+    // Register the ignore_action as the handler for SIGTERM this signal will be ignored.
+    sigaction(SIGTERM, &ignore_action, NULL);
 
     while (1) {
 
         char **usr_input = malloc(sizeof(char *) * MAX_LINE);
         char *usr_input_str = malloc(sizeof(char) * MAX_LINE);
         struct command_data *curr_command = malloc(sizeof(struct command_data));
+        // Initialize values to NULL
+        curr_command->command = NULL;
+        curr_command->argumentList = NULL;
+        curr_command->input_file = NULL;
+        curr_command->output_file = NULL;
+        curr_command->run_in_background = 0;
         char *pidString = NULL;
         int n = 0, i = 0, status = 0;
         size_t max_line1 = MAX_LINE, *max_line = &max_line1, bytes_read = 0, alloc_mem = sizeof(char) * MAX_LINE,
@@ -258,28 +329,34 @@ void shellUserInput(void) {
        // parse input into command struct.
        parseCommand(usr_input_str, curr_command);
 
-       printf("command: %s\narguments: %s\ninput: %s\noutput: %s\nbackground %d\n",
-              curr_command->command, curr_command->argumentList, curr_command->input_file, curr_command->output_file,
-              curr_command->run_in_background);
+       //printf("command: %s\narguments: %s\ninput: %s\noutput: %s\nbackground %d\n",
+//              curr_command->command, curr_command->argumentList, curr_command->input_file, curr_command->output_file,
+//              curr_command->run_in_background);
 
-       status = processUserInput(usr_input_str, bytes_read);
-       printf("The string the user entered was: '%s' and it was %d bytes long.\n", usr_input_str, (int)bytes_read);
-
-       if (status == 0){
-           return;
-       }
+       status = processUserInput(curr_command);
+       //printf("The string the user entered was: '%s' and it was %d bytes long.\n", usr_input_str, (int)bytes_read);
 
        i++;
-       if (i == 10) {
-           free(curr_command->command);
-           free(curr_command->argumentList);
-           free(curr_command->input_file);
-           free(curr_command->output_file);
+       if (i == 10 || status == 1) {
+           if (curr_command->command != NULL){
+               free(curr_command->command);
+           }
+           if (curr_command->argumentList != NULL){
+               free(curr_command->argumentList);
+           }
+           if (curr_command->input_file != NULL) {
+               free(curr_command->input_file);
+           }
+           if (curr_command->output_file != NULL) {
+               free(curr_command->output_file);
+           }
            free(curr_command);
-           free(pidString);
+           if (pidString != NULL) {
+               free(pidString);
+           }
+
            free(*usr_input);
            free(usr_input_str);
-           free(max_line);
            break;
        }
 
